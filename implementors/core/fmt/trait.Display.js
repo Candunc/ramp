b2b8cca79c5b0976d1e2760de211b4ab49b4ec28
Travis CI User <travis@example.org>
2017-10-02 03:39:09 +0000

    Documentation for Aatch/ramp@7525e9e

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ramp"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"ramp/ll/limb/struct.Limb.html\" title=\"struct ramp::ll::limb::Limb\">Limb</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"ramp/int/struct.Int.html\" title=\"struct ramp::int::Int\">Int</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"ramp/int/struct.ParseIntError.html\" title=\"struct ramp::int::ParseIntError\">ParseIntError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"ramp/rational/struct.ParseRationalError.html\" title=\"struct ramp::rational::ParseRationalError\">ParseRationalError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"ramp/rational/struct.Rational.html\" title=\"struct ramp::rational::Rational\">Rational</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ ramp/int/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Int",""],["ParseIntError","Error that arises when parsing an [`Int`]."]],"trait":[["RandomInt","Trait for generating random `Int`s."]]});
